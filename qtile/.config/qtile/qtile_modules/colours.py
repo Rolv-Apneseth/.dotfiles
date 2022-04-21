906602158f7e90cb738395877c59de8f10b9c84a
Rolv Apneseth <rolv.apneseth@gmail.com>
2022-04-21 19:44:51 +0100

    Adjusted qtile purple colour

--- qtile/.config/qtile/qtile_modules/colours.py
+++ qtile/.config/qtile/qtile_modules/colours.py
@@ -2,8 +2,8 @@ COLOURS = dict(
     transparent="#00000000",
     white="#FFFFFF",
     black="#000000",
-    purple="#FF00FF",
-    purple_light="#FF60FF",
+    purple="#BB77BB",
+    purple_light="#DD77DD",
     cyan="#00FFFF",
     graph_line="#d4d40b",
 )
