1e268d89a57dc7577f74a99a3d88b4964480821e
Rolv Apneseth <rolv.apneseth@gmail.com>
2022-07-13 13:46:26 +0100

    Updated the ensure_installed option for nvim-treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ local constants = require("core.constants")
 local keymaps_text_object = constants.OTHER_KEYMAPPINGS.treesitter.text_object
 
 configs.setup({
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = "all", -- "all", or a list of languages
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
     ignore_install = { "" }, -- List of parsers to ignore installing
     highlight = {
