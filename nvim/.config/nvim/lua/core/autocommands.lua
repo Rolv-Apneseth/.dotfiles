a8eb20b4e74965020060f5f37efaaa84ea3da4b6
rolv <rolv.apneseth@gmail.com>
2022-11-24 10:48:23 +0000

    feat: add ability to exclude filetypes from auto format, currently only ignoring .txt files

--- nvim/.config/nvim/lua/core/autocommands.lua
+++ nvim/.config/nvim/lua/core/autocommands.lua
@@ -4,9 +4,18 @@ local auto_cmd = vim.api.nvim_create_autocmd
 
 local group_general_settings = augroup("_general_settings", { clear = true })
 
--- Format on save (with lsp)
+-- Format on save (with LSP)
+local excluded_filetypes = { "text" }
 auto_cmd("BufWritePre", {
     callback = function()
+        -- Exclude certain filetypes
+        for _, filetype in ipairs(excluded_filetypes) do
+            if vim.bo.filetype == filetype then
+                return
+            end
+        end
+
+        -- LSP format
         vim.lsp.buf.format()
     end,
     group = augroup("lsp_document_format", { clear = true }),
