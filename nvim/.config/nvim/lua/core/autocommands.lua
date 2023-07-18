a4c42268f1a9d37536375d27c5c376b0e6964ead
rolv <rolv.apneseth@gmail.com>
2023-07-19 00:53:21 +0100

    nvim: add BufWritePost hook for running svgo on svg files

--- nvim/.config/nvim/lua/core/autocommands.lua
+++ nvim/.config/nvim/lua/core/autocommands.lua
@@ -4,7 +4,7 @@ local auto_cmd = vim.api.nvim_create_autocmd
 local group_general_settings = augroup("_general_settings", { clear = true })
 
 -- Format on save (with LSP)
-local excluded_filetypes = { "text" }
+local excluded_filetypes = { "text", "svg" }
 auto_cmd("BufWritePre", {
     callback = function()
         -- Exclude certain filetypes
@@ -18,6 +18,16 @@ auto_cmd("BufWritePre", {
         vim.lsp.buf.format()
     end,
     group = augroup("lsp_document_format", { clear = true }),
+})
+
+-- Custom command execution after save
+auto_cmd("BufWritePost", {
+    callback = function()
+        if vim.bo.filetype == "svg" then
+            vim.cmd("!svgo %")
+        end
+    end,
+    group = augroup("post_write_custom_commands", { clear = true }),
 })
 
 -- Q to close
