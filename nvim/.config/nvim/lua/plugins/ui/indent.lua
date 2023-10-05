60b2d08ddd813f231f92baf30a2dbf7c57620529
rolv <rolv.apneseth@gmail.com>
2023-10-05 12:50:19 +0100

    nvim: keep indent-blankline at v2

--- nvim/.config/nvim/lua/plugins/ui/indent.lua
+++ nvim/.config/nvim/lua/plugins/ui/indent.lua
@@ -1,5 +1,6 @@
 return {
     "lukas-reineke/indent-blankline.nvim", -- for showing lines at different indentation levels
+    version = "2.20.8",
     opts = {
         space_char_blankline = " ",
         show_current_context = true,
