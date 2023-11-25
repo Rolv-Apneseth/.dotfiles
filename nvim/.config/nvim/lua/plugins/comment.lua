6643e2b60a827b0c12412fbeafbc5552475c3dbf
rolv <rolv.apneseth@gmail.com>
2023-11-25 01:07:33 +0000

    feat(nvim): include comment.nvim config

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -2,6 +2,42 @@ return {
     {
         "numToStr/Comment.nvim", -- comment out lines
         opts = {
+            ---Add a space b/w comment and the line
+            padding = true,
+            ---Whether the cursor should stay at its position
+            sticky = true,
+            ---Lines to be ignored while (un)comment
+            ignore = nil,
+            ---LHS of toggle mappings in NORMAL mode
+            toggler = {
+                ---Line-comment toggle keymap
+                line = "gcc",
+                ---Block-comment toggle keymap
+                block = "gbc",
+            },
+            ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+            opleader = {
+                ---Line-comment keymap
+                line = "gc",
+                ---Block-comment keymap
+                block = "gb",
+            },
+            ---LHS of extra mappings
+            extra = {
+                ---Add comment on the line above
+                above = "gcO",
+                ---Add comment on the line below
+                below = "gco",
+                ---Add comment at the end of line
+                eol = "gcA",
+            },
+            --- Keybindings
+            mappings = {
+                ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+                basic = true,
+                ---Extra mapping; `gco`, `gcO`, `gcA`
+                extra = true,
+            },
             pre_hook = function(ctx)
                 local utils = require("Comment.utils")
                 local ts_utils = require("ts_context_commentstring.utils")
