af2dd636045ed66cbbcd75a57fde3b824d0c3791
rolv <rolv.apneseth@gmail.com>
2023-11-25 01:06:36 +0000

    fix(nvim): disable gc keybind from nvim-various-textobjs

--- nvim/.config/nvim/lua/plugins/various_textobjs.lua
+++ nvim/.config/nvim/lua/plugins/various_textobjs.lua
@@ -2,8 +2,8 @@ return {
     "chrisgrieser/nvim-various-textobjs",
     opts = {
         lookForwardSmall = 5, -- lines to seek forwards for "small" textobjs (mostly characterwise textobjs)
-        lookForwardBig = 25,  -- lines to seek forwards for "big" textobjs (mostly linewise textobjs)
+        lookForwardBig = 25, -- lines to seek forwards for "big" textobjs (mostly linewise textobjs)
         useDefaultKeymaps = true,
-        disabledKeymaps = {}, -- disable some default keymaps, e.g. { "ai", "ii" }
+        disabledKeymaps = { "gc" }, -- disable some default keymaps, e.g. { "ai", "ii" }
     },
 }
