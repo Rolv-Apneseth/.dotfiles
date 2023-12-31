5cd843a345128e57d6d12f5ec016d5ea09cd8eef
rolv <rolv.apneseth@gmail.com>
2023-12-31 21:58:59 +0000

    nvim: update indent-blankline config

--- nvim/.config/nvim/lua/plugins/ui/indent.lua
+++ nvim/.config/nvim/lua/plugins/ui/indent.lua
@@ -1,10 +1,39 @@
+-- :h ibl.config
 return {
     "lukas-reineke/indent-blankline.nvim", -- for showing lines at different indentation levels
-    version = "2.20.8",
+    main = "ibl",
     opts = {
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-        filetype_exclude = { "alpha" },
+        enabled = true,
+        scope = {
+            enabled = true,
+            show_start = false,
+            show_end = false,
+            show_exact_scope = false,
+            injected_languages = false,
+            highlight = { "Label" },
+            priority = 500,
+
+            include = {
+                node_type = {
+                    ["*"] = {
+                        "return_statement",
+                        "switch_statement",
+                        "table_constructor",
+                    },
+                },
+            },
+        },
+        indent = {
+            char = "│",
+            highlight = "Comment",
+            smart_indent_cap = false,
+        },
+        whitespace = {
+            remove_blankline_trail = true,
+        },
+        --[[ space_char_blankline = " ", ]]
+        --[[ show_current_context = true, ]]
+        --[[ show_current_context_start = true, ]]
+        exclude = { filetypes = { "alpha" } },
     },
 }
