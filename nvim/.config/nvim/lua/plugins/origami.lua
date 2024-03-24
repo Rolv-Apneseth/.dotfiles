b95973136d5206a5ed8c0964975f667b7c478a51
rolv <rolv.apneseth@gmail.com>
2024-03-24 23:36:05 +0000

    refactor(nvim): drop folding plugins (`ufo` and `origami`) due to lack of usage of folds

--- nvim/.config/nvim/lua/plugins/origami.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "chrisgrieser/nvim-origami", -- Open / close folds with "h" and "l"
-    event = "BufReadPost", -- later or on keypress would prevent saving folds
-    opts = {
-        keepFoldsAcrossSessions = true,
-        pauseFoldsOnSearch = true,
-        setupFoldKeymaps = true,
-    },
-}
