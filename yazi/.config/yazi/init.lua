d6b76fe16ba1c7c1f6bc76445b598853b4f9ee39
rolv <rolv.apneseth@gmail.com>
2024-05-16 14:55:32 +0100

    feat(yazi): update config to use new `ya pack` plugin manager

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,91 +1,41 @@
-PLUGINS = {
-    "Reledia/glow.yazi",
-    "DreamMaoMao/keyjump.yazi",
-    "Sonico98/exifaudio.yazi",
-    "ndtoan96/ouch.yazi",
-    "dedukun/bookmarks.yazi",
-    "DreamMaoMao/fg.yazi",
-    "Rolv-Apneseth/bypass.yazi",
-    "Rolv-Apneseth/starship.yazi",
-}
-PLUGINS_DIR = "/home/rolv/.config/yazi/plugins/"
-DELETE_OLD_PLUGINS = true
-
--- UTILS
-local function build_repo_url(slug)
-    return "https://github.com/" .. slug .. ".git"
-end
-
-local function split_slug(slug)
-    local t = {}
-    for str in string.gmatch(slug, "[^/]+") do
-        table.insert(t, str)
-    end
-    return t
-end
-
-local function build_repo_path(name)
-    return PLUGINS_DIR .. name .. "/"
-end
-
-local function repo_exists_locally(path)
-    local ok, _err, code = os.rename(path, path)
-    if not ok then
-        if code == 13 then
-            -- Permission denied, but it exists
-            return true
-        end
-    end
-    return ok
-end
-
--- INSTALL PLUGINS
-local names = {}
-for i, slug in ipairs(PLUGINS) do
-    local url = build_repo_url(slug)
-    local name = split_slug(slug)[2]
-    local path = build_repo_path(name)
-    names[i] = name
-
-    if not repo_exists_locally(path) then
-        os.execute("git clone " .. url .. " " .. path)
-    end
-end
-
--- REMOVE OLD PLUGINS
-if DELETE_OLD_PLUGINS then
-    local pfile = io.popen('ls "' .. PLUGINS_DIR .. '"')
-    for filename in pfile:lines() do
-        local found = false
-        for i, name in ipairs(names) do
-            if name == filename then
-                found = true
-                goto continue
-                table.remove(names, i)
-            end
-        end
-        ::continue::
-
-        if not found then
-            os.execute('rm -rf "' .. PLUGINS_DIR .. filename .. '"')
-        end
-    end
-    pfile:close()
-end
-
 -- PLUGIN SETUP/CONFIG
-require("starship"):setup()
-require("bookmarks"):setup({
-    notify = {
-        enable = true,
-        timeout = 1,
-        message = {
-            new = " Created bookmark '<key>'",
-            delete = "󰧌 Deleted bookmark '<key>'",
-            delete_all = "󰧌 Deleted all bookmarks",
+--- Require plugin, giving a warning notification if a plugin is not found
+--- @param plugin_name string
+---@return nil|any
+local function require_plugin(plugin_name)
+    local status_ok, plugin = pcall(require, plugin_name)
+
+    if not status_ok then
+        ya.notify({
+            title = "Plugins",
+            content = "Could not find plugin: " .. plugin_name,
+            timeout = 5,
+        })
+        return nil
+    end
+
+    return plugin
+end
+
+local starship = require_plugin("starship")
+if starship then
+    starship:setup()
+end
+
+local bookmarks = require_plugin("bookmarks")
+if bookmarks then
+    bookmarks:setup({
+        notify = {
+            enable = true,
+            timeout = 1,
+            message = {
+                new = " Created bookmark '<key>'",
+                delete = "󰧌 Deleted bookmark '<key>'",
+                delete_all = "󰧌 Deleted all bookmarks",
+            },
         },
-    },
-})
+    })
+end
 
 -- OVERRIDE DEFAULT UI
 -- Give some extra space for footer and header + cool columns
