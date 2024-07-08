e5d604bf11c19a2c9be88b268c18180b7272ad56
rolv <rolv.apneseth@gmail.com>
2024-07-08 14:48:27 +0100

    fix(nvim): remove deprecated `gitsigns` config options

--- nvim/.config/nvim/lua/plugins/ui/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/ui/gitsigns.lua
@@ -6,34 +6,19 @@ return {
     opts = {
         signs = {
             add = {
-                hl = "GitSignsAdd",
                 text = icons.AddOrChange,
-                numhl = "GitSignsAddNr",
-                linehl = "GitSignsAddLn",
             },
             change = {
-                hl = "GitSignsChange",
                 text = icons.AddOrChange,
-                numhl = "GitSignsChangeNr",
-                linehl = "GitSignsChangeLn",
             },
             delete = {
-                hl = "GitSignsDelete",
                 text = icons.Delete,
-                numhl = "GitSignsDeleteNr",
-                linehl = "GitSignsDeleteLn",
             },
             topdelete = {
-                hl = "GitSignsDelete",
                 text = icons.Delete,
-                numhl = "GitSignsDeleteNr",
-                linehl = "GitSignsDeleteLn",
             },
             changedelete = {
-                hl = "GitSignsChange",
                 text = icons.AddOrChange,
-                numhl = "GitSignsChangeNr",
-                linehl = "GitSignsChangeLn",
             },
         },
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
@@ -45,15 +30,11 @@ return {
             follow_files = true,
         },
         attach_to_untracked = true,
-        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
             virt_text = true,
             virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
             delay = 1000,
             ignore_whitespace = false,
-        },
-        current_line_blame_formatter_opts = {
-            relative_time = false,
         },
         sign_priority = 6,
         update_debounce = 100,
@@ -66,9 +47,6 @@ return {
             relative = "cursor",
             row = 0,
             col = 1,
-        },
-        yadm = {
-            enable = false,
         },
     },
 }
