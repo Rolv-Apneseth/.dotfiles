20c886a933eb20653223b2b8b8c37f4f842bddfa
rolv <rolv.apneseth@gmail.com>
2024-07-16 01:32:49 +0100

    feat(nvim): add plugin to move LSP inlay hints to the end of the line

---
+++ nvim/.config/nvim/lua/plugins/code/inlay_hints.lua
@@ -0,0 +1,16 @@
+return {
+    "chrisgrieser/nvim-lsp-endhints",
+    event = "LspAttach",
+    opts = {
+        icons = {
+            type = "󰁕 ",
+            parameter = "",
+        },
+        label = {
+            padding = 0,
+            marginLeft = 0,
+            bracketedParameters = true,
+        },
+        autoEnableHints = true,
+    },
+}
