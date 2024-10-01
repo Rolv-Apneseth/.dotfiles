eac4769354a88fb0daebe1ef05b2b75c3c6d1033
rolv <rolv.apneseth@gmail.com>
2024-10-01 20:24:57 +0100

    feat(nvim): add plugins for enhanced quickfix and help menus

---
+++ nvim/.config/nvim/lua/plugins/ui/helpview.lua
@@ -0,0 +1,4 @@
+return {
+    "OXY2DEV/helpview.nvim", -- Better `vimdoc/help`
+    ft = "help", -- Not recommended by the author - change to `lazy = false` if there's issues
+}
---
+++ nvim/.config/nvim/lua/plugins/ui/quicker.lua
@@ -0,0 +1,23 @@
+return {
+    "stevearc/quicker.nvim", -- Better quickfix list
+    lazy = true,
+    ft = "qf",
+    opts = {
+        keys = {
+            {
+                ">",
+                function()
+                    require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+                end,
+                desc = "Expand quickfix context",
+            },
+            {
+                "<",
+                function()
+                    require("quicker").collapse()
+                end,
+                desc = "Collapse quickfix context",
+            },
+        },
+    },
+}
