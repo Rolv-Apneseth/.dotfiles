77607dbf643122275bc09e47964dbbf285c1ebe7
rolv <rolv.apneseth@gmail.com>
2024-10-08 17:25:45 +0100

    fix(nvim): use all features flag for cargo in rust projects

--- nvim/.config/nvim/lua/plugins/code/rust.lua
+++ nvim/.config/nvim/lua/plugins/code/rust.lua
@@ -31,6 +31,7 @@ return {
                         },
                         cargo = {
                             autoReload = true,
+                            features = "all",
                         },
                         procMacro = {
                             ignored = {
