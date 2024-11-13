eacbbd0cc0f49d0c4cd37dc4019be39be28660e2
rolv <rolv.apneseth@gmail.com>
2024-11-13 15:44:10 +0000

    refactor(nvim): change `leap.nvim` options, use a single keybind to search up or down

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -2,15 +2,24 @@ local require_plugin = require("core.helpers").require_plugin
 
 return {
     "ggandor/leap.nvim", -- for easier navigation around a file
+
     config = function()
         local leap = require_plugin("leap")
         if not leap then
             return
         end
 
-        leap.add_default_mappings()
-
-        -- Further styling set in theme, with groups LeapBackDrop and LeapMatch
-        require("leap").opts.highlight_unlabeled_phase_one_targets = true
+        leap.opts.special_keys.prev_target = "<s-enter>"
+        leap.opts.highlight_unlabeled_phase_one_targets = true
     end,
+
+    keys = {
+        {
+            "s",
+            function()
+                require("leap").leap({ target_windows = { vim.fn.win_getid() } })
+            end,
+            desc = "Todo comments",
+        },
+    },
 }
