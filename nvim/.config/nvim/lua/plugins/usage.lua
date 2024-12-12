17d114dc373a2e20be08547d6e6a37f12bc68293
rolv <rolv.apneseth@gmail.com>
2024-12-12 20:44:08 +0000

    refactor(nvim): `pendulum.lua` -> `usage.lua`

--- nvim/.config/nvim/lua/plugins/usage.lua
+++ nvim/.config/nvim/lua/plugins/usage.lua
@@ -5,10 +5,18 @@ return {
         log_file = vim.fn.expand("$XDG_DATA_HOME/pendulum-log.csv"),
         top_n = 5,
         gen_reports = true, -- Requires Go installed
+        report_section_excludes = {
+            "branch",
+        },
+        report_excludes = {
+            project = {
+                "unknown_project", -- Exclude unknown (non-git) projects
+            },
+        },
     },
     -- Rebuild binary when installing/updating
     build = "<cmd>PendulumRebuild<cr>",
     keys = {
-        { "<leader>O", "<cmd>Pendulum<cr>", desc = "Usage stats" },
+        { "<leader>O", "<cmd>Pendulum ''<cr>", desc = "Usage stats" },
     },
 }
