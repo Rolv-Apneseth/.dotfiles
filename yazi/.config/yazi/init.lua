6df35b2636e26f2f89f56df3eb0aac2a22fa688e
rolv <rolv.apneseth@gmail.com>
2024-12-22 14:06:54 +0000

    fix(yazi): update for `v0.4`

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -40,21 +40,21 @@ end
 -- OVERRIDE DEFAULT UI
 -- Give some extra space for footer and header + cool columns
 local old_build = Tab.build
+
 Tab.build = function(self, ...)
     local bar = function(c, x, y)
         if x <= 0 or x == self._area.w - 1 then
-            return ui.Bar(ui.Rect.default, ui.Bar.TOP)
+            return ui.Bar(ui.Bar.TOP):area(ui.Rect.default)
         end
 
-        return ui.Bar(
-            ui.Rect({
+        return ui.Bar(ui.Bar.TOP)
+            :area(ui.Rect({
                 x = x,
                 y = math.max(0, y),
                 w = ya.clamp(0, self._area.w - x, 1),
                 h = math.min(1, self._area.h),
-            }),
-            ui.Bar.TOP
-        ):symbol(c)
+            }))
+            :symbol(c)
     end
 
     local c = self._chunks
@@ -66,14 +66,13 @@ Tab.build = function(self, ...)
 
     local style = THEME.manager.border_style
     self._base = ya.list_merge(self._base or {}, {
-        --[[ ui.Border(self._area, ui.Border.ALL):type(ui.Border.ROUNDED):style(style), ]]
-        ui.Bar(self._chunks[1], ui.Bar.RIGHT):style(style),
-        ui.Bar(self._chunks[3], ui.Bar.LEFT):style(style),
+        ui.Bar(ui.Bar.RIGHT):area(self._chunks[1]):style(style),
+        ui.Bar(ui.Bar.LEFT):area(self._chunks[1]):style(style),
 
         bar("┬", c[1].right - 1, c[1].y),
         bar("┴", c[1].right - 1, c[1].bottom - 1),
         bar("┬", c[2].right, c[2].y),
-        bar("┴", c[2].right, c[1].bottom - 1),
+        bar("┴", c[2].right, c[2].bottom - 1),
     })
 
     old_build(self, ...)
